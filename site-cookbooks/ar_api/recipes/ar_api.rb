2f89aae12dd08848e3043bd9d8b7c606c03de6dc
euonymus <euonymus0220@gmail.com>
2018-03-12 16:05:41 +0900

    rename dir

--- site-cookbooks/ar_api/recipes/ar_api.rb
+++ site-cookbooks/ar_api/recipes/ar_api.rb
@@ -1,13 +1,13 @@
 pip_requirements "#{node[:ar_api][:app_root]}/requirements.txt"
 
 # delete a uploads directory
-directory node[:ar_api][:app_root] + "/static/uploads" do
+directory node[:ar_api][:app_root] + "/static/uploaded" do
   recursive true
   action :delete
 end
 
 # create a uploads directory
-directory node[:ar_api][:app_root] + '/static/uploads' do
+directory node[:ar_api][:app_root] + '/static/uploaded' do
   owner "www-data"
   group "www-data"
   mode "0775"
