fd7d3e6f7364a5c128137a3747d04a7af7df1a4d
euonymus <euonymus0220@gmail.com>
2018-03-12 15:07:54 +0900

    optimize

---
+++ site-cookbooks/ar_api/metadata.rb
@@ -0,0 +1,10 @@
+name             'ar_api'
+maintainer       'euonymus'
+maintainer_email 'euonymus0220@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures ar_api'
+long_description 'Installs/Configures ar_api'
+version          '0.1.0'
+
+depends 'apache2'
+depends 'poise-python'
---
+++ site-cookbooks/ar_api/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: ar_api
+# Recipe:: default
+#
+# Copyright (C) 2018 euonymus
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'ar_api::apache'
+include_recipe 'ar_api::ar_api'
